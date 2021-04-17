912fcd318a5df95b741d6515d5bdca40d1e08431
adrian <fak.adrian@gmail.com>
2021-04-17 02:35:02 +0200

    add list of profiles and profile details frontend

---
+++ client/src/app/interceptors/jwt.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AccountService } from '../services/account.service';
+import { User } from '../models/User';
+import { take } from 'rxjs/operators';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private accountService: AccountService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let currentUser: User
+    this.accountService.currentUser$.pipe(take(1)).subscribe((user: User)=>{   //it will complete after taking 1 value so no need to unsubcribe
+      currentUser = user
+      if(currentUser){
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${user.token}`
+          }
+        })
+      }
+    })   
+    return next.handle(request);
+  }
+}
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
+import { Member } from 'src/app/models/Member';
+import { Photo } from 'src/app/models/photo';
+import { MembersService } from 'src/app/services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.scss']
 })
 export class MemberDetailComponent implements OnInit {
-
-  constructor() { }
+  member: Member
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember()
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ]
+ 
+  }
+
+  getImages(): NgxGalleryImage[]{
+    const imageUrls = []
+    this.member.photos.forEach((photo: Photo)=>{
+      imageUrls.push({
+        small: photo?.url,
+        medium: photo?.url,
+        big: photo?.url
+      })
+    })
+    return imageUrls
+  }
+
+  loadMember(){
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe((member: Member)=> {
+      this.member = member
+      this.galleryImages = this.getImages()
+    })
   }
 
 }
